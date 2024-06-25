1d595958f0d50972649aa3cecd4e23c2afdd4ffa
Enza Rafaela De Nadai Victal Mariano <rafaeladenadai@gmail.com>
2024-06-25 10:44:03 -0300

    feat: adiciona novo endpoint

--- main.py
+++ main.py
@@ -15,6 +15,12 @@ async def translate_text(item: TextToTranslate):
     traducao = tradutor.translate(item.text)
     return {"translated_text": traducao}
 
+@app.get("/translate/{text}/{target_language}")
+async def translate_text(text: str, target_language: str):
+    tradutor = GoogleTranslator(source= "auto", target= target_language)
+    traducao = tradutor.translate(text)
+    return {"translated_text": traducao}
+
 @app.get("/")
 async def read_root():
     return {"message": "Welcome to the translation API!"}
